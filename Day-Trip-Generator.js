c5d5ed6abf2ace60bdb6466b9d44da2990d6636a
Timothy Casey <tvcasey@yahoo.com>
2020-09-10 20:30:16 -0400

    The beginnig of the process.  Random array identifier completed.  Will use extensively with every problem.

---
+++ Day-Trip-Generator.js
@@ -0,0 +1,143 @@
+
+/*  Need to make good consistent solid commits [5 points]*/
+
+/*  As a user, I want a destination to be randomly
+selected for my day trip. [5 points]
+1. Need a for loop of an array of destinations.
+2. Need to find the right variables and procedure
+   to get a random integer to choose the array.
+3. Make an alert() to provide the info.  Maybe a button to roll
+   the random destination. */
+
+   function choice(cities)
+{
+return cities[Math.floor(Math.random() * cities.length)];
+   
+}
+   var cities = ["Paris", "London", "Rome", "Berlin", "Madrid", "Athens", "Budapest", "Moscow", "Zurich"];
+    console.log(choice(cities));
+{
+
+
+}
+
+/*  Got the information for this random output of an array from
+    www.w3resource.com.  "JavaScript: Get a random item from an array."
+    It also works with numbers from an array.  */
+
+
+
+/*  As a user, I want a restauerant to be randomly selected for my day trip. [5 points]
+
+
+*/
+
+
+
+
+/*  As a user I want a mode of transportation to be randomly selected for my day trip [5 points]*/
+
+
+
+
+
+
+
+
+
+/*  As a user, I want a form of entertainment to be randomly selected for my day trip. [5 points]*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*  As a user, I want to be able to randomly re-select a destination, restaurant, mode of transportation,
+and/or form of entertainment if I don't like one or more of those things. [15 points] */
+
+
+
+
+
+
+
+
+
+
+
+
+/*  As a user, I want to be able to confirm that my day trip is "complete" once I like all of the random
+selections.  [10 points] */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*  As a user, I want to display my completed trip in the console. [10 points] */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*  As a developer, I want all of my functions to have a Single Responsibility.  Remember,
+each function should do just one thing! [5 points]  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
